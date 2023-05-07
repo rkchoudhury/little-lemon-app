15c56eb6647f9031b9cca18c2a4aa86bc9960c1c
Rakesh Choudhury <rakesh.choudhury@honeywell.com>
2023-05-07 21:36:14 +0530

    Changed the file name to TestActivity

--- LittleLemonLogin/app/src/main/java/com/example/littlelemonlogin/TestActivity.kt
+++ LittleLemonLogin/app/src/main/java/com/example/littlelemonlogin/TestActivity.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.littlelemonlogin.ui.theme.LittleLemonLoginTheme
 
-class DashboardActivity : ComponentActivity() {
+class TestActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
