4e99f80b34f61297c3985e52e40bbc8947428619
Rakesh Choudhury <rakesh.choudhury@honeywell.com>
2023-05-14 19:09:58 +0530

    Added Home and MenuList screens

---
+++ jetpackNavigation/app/src/main/java/com/example/myapplication/HomeScreen.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HomeScreen() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Home", fontSize = 32.sp)
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Press Me")
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PreviewHomeScreen() {
+    HomeScreen()
+}
---
+++ jetpackNavigation/app/src/main/java/com/example/myapplication/MenuListScreen.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MenuListScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Menu List", fontSize = 32.sp)
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PreviewMenuLIst() {
+    MenuListScreen()
+}
