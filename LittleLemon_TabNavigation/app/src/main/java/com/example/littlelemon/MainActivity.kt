c84673d5961ab917f71245d64fbdd41db7d5c751
Rakesh Choudhury <rakesh.choudhury@honeywell.com>
2023-05-21 20:06:05 +0530

    Handle bottom navigation logics and tab options

--- LittleLemon_TabNavigation/app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ LittleLemon_TabNavigation/app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -5,9 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -49,5 +52,34 @@ fun MyApp() {
 
 @Composable
 fun MyBottomNavigation(navController: NavController) {
+    //destinationList: contains list of tab options
+    val destinationList = listOf(
+        Home,
+        Menu,
+        Location
+    )
+    val selectedIndex = rememberSaveable {
+        mutableStateOf(0)
+    }
 
+    BottomNavigation {
+        destinationList.forEachIndexed { index, destination ->
+            BottomNavigationItem(
+                label = { Text(text = destination.title) },
+                icon = {
+                    Icon(
+                        painter = painterResource(id = destination.icon),
+                        contentDescription = destination.title
+                    )
+                },
+                selected = index == selectedIndex.value,
+                onClick = {
+                    selectedIndex.value = index
+                    navController.navigate(destinationList[index].route) {
+                        popUpTo(Home.route)
+                        launchSingleTop = true
+                    }
+                })
+        }
+    }
 }
