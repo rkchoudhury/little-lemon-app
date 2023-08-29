619dcc82962335c8027bb1780a027cef043cd0ad
Rakesh Choudhury <rakesh.choudhury@honeywell.com>
2023-08-30 05:22:22 +0530

    Added animations

--- Animations/app/src/main/java/com/example/animations/MainActivity.kt
+++ Animations/app/src/main/java/com/example/animations/MainActivity.kt
@@ -3,11 +3,20 @@ package com.example.animations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.animations.ui.theme.AnimationsTheme
@@ -22,7 +31,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+                        GreetingWithoutAnimations()
+                        GreetingDefaultAnimations()
+                        GreetingWithFadeAnimations()
+                        GreetingWithFadeAnimationsTime()
+                    }
                 }
             }
         }
@@ -30,14 +44,93 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun GreetingWithoutAnimations() {
+    var visible by remember {
+        mutableStateOf(true)
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Button(onClick = { visible = !visible }) {
+            Text("Button")
+        }
+        if (visible) {
+            Text(text = "Hello, world!")
+        }
+    }
+}
+
+@Composable
+fun GreetingDefaultAnimations() {
+    var visible by remember {
+        mutableStateOf(true)
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { visible = !visible }) {
+            Text("Button")
+        }
+        AnimatedVisibility(visible = visible) {
+            Text(text = "Hello, world!")
+        }
+    }
+}
+
+@Composable
+fun GreetingWithFadeAnimations() {
+    var visible by remember {
+        mutableStateOf(true)
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { visible = !visible }) {
+            Text("Button")
+        }
+        AnimatedVisibility(
+            visible = visible,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            Text(text = "Hello, world!")
+        }
+    }
+}
+
+@Composable
+fun GreetingWithFadeAnimationsTime() {
+    var visible by remember {
+        mutableStateOf(true)
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { visible = !visible }) {
+            Text("Button")
+        }
+        AnimatedVisibility(
+            visible = visible,
+            enter = fadeIn(animationSpec = tween(2000)),
+            exit = fadeOut(animationSpec = tween(2000))
+        ) {
+            Text(text = "Hello, world!")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AnimationsTheme {
-        Greeting("Android")
+        GreetingWithoutAnimations()
     }
 }
