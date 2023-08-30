624df73846879a016ac57f41f10758190be2c047
Rakesh Choudhury <rakesh.choudhury@honeywell.com>
2023-08-30 20:47:29 +0530

    Added Lower Panel list

--- UserInterfaceFinalProject/app/src/main/java/com/example/littlelemon/LowerPanel.kt
+++ UserInterfaceFinalProject/app/src/main/java/com/example/littlelemon/LowerPanel.kt
@@ -1,14 +1,16 @@
 package com.example.littlelemon
 
 import android.util.Log
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -33,7 +35,7 @@ fun WeeklySpecialCard() {
     ) {
         Text(
             text = stringResource(R.string.weekly_special),
-            style = MaterialTheme.typography.h1,
+            style = MaterialTheme.typography.h5,
             modifier = Modifier
                 .padding(8.dp)
         )
@@ -47,7 +49,28 @@ fun MenuDish(navController: NavHostController? = null, dish: Dish) {
         Log.d("AAA", "Click ${dish.id}")
         navController?.navigate(DishDetails.route + "/${dish.id}")
     }) {
-        //TODO: Insert code here
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Column {
+                Text(text = dish.name, style = MaterialTheme.typography.subtitle1)
+                Text(
+                    text = dish.description,
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier
+                        .padding(0.dp, 5.dp)
+                        .fillMaxWidth(0.75f)
+                )
+                Text(text = "$${dish.price}", style = MaterialTheme.typography.body2)
+            }
+            Image(
+                painter = painterResource(dish.imageResource),
+                contentDescription = "Dish image",
+                modifier = Modifier.clip(RoundedCornerShape(10.dp)),
+            )
+        }
     }
     Divider(
         modifier = Modifier.padding(start = 8.dp, end = 8.dp),
