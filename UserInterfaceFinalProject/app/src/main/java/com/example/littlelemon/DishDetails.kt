cdfc97d0ff9f4711d47fdb1047cccedf97c9fd3e
Rakesh Choudhury <rakesh.choudhury@honeywell.com>
2023-08-31 05:49:01 +0530

    Added DishDetails contents

--- UserInterfaceFinalProject/app/src/main/java/com/example/littlelemon/DishDetails.kt
+++ UserInterfaceFinalProject/app/src/main/java/com/example/littlelemon/DishDetails.kt
@@ -1,12 +1,18 @@
 package com.example.littlelemon
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -14,7 +20,27 @@ import androidx.compose.ui.unit.dp
 fun DishDetails(id: Int) {
     val dish = requireNotNull(DishRepository.getDish(id))
     Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
-        //TODO: Insert code here
+        TopAppBar()
+        Image(
+            painter = painterResource(id = dish.imageResource),
+            contentDescription = "dish image",
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
+        Column(
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            modifier = Modifier.padding(10.dp, 0.dp, 10.dp, 0.dp)
+        ) {
+            Text(text = dish.name, style = MaterialTheme.typography.h1)
+            Text(text = dish.description, style = MaterialTheme.typography.body1)
+            Counter()
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth()) {
+                Text(
+                    text = stringResource(id = R.string.add_for) + " $${dish.price}",
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
     }
 }
 
@@ -55,7 +81,7 @@ fun Counter() {
     }
 }
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
 fun DishDetailsPreview() {
     DishDetails(id = 1)
