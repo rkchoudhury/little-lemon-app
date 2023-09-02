5a61b644a12cdb6b666c6110d81c5f16c583025a
Rakesh Choudhury <rakesh.choudhury@honeywell.com>
2023-09-02 18:15:38 +0530

    Added valued animations

--- Animations/app/src/main/java/com/example/animations/MainActivity.kt
+++ Animations/app/src/main/java/com/example/animations/MainActivity.kt
@@ -4,21 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.animations.ui.theme.AnimationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,12 +34,40 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Row(horizontalArrangement = Arrangement.SpaceEvenly) {
-                        GreetingWithoutAnimations()
-                        GreetingDefaultAnimations()
-                        GreetingWithFadeAnimations()
-                        GreetingWithFadeAnimationsTime()
+                    Column {
+                        Text(
+                            text = "Fade In/Out Animations",
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(0.dp, 20.dp),
+                            fontWeight = FontWeight(500)
+                        )
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceEvenly,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            GreetingWithoutAnimations()
+                            GreetingDefaultAnimations()
+                            GreetingWithFadeAnimations()
+                            GreetingWithFadeAnimationsTime()
+                        }
+                        Divider(
+                            color = Color.Black,
+                            thickness = 2.dp,
+                            modifier = Modifier.padding(0.dp, 20.dp)
+                        )
+                        Text(
+                            text = "Valued Animation \n\n Click on the image to see the animation",
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(0.dp, 20.dp),
+                            fontWeight = FontWeight(500)
+                        )
+                        ValuedImageAnimations()
                     }
+
                 }
             }
         }
@@ -124,6 +155,35 @@ fun GreetingWithFadeAnimationsTime() {
         ) {
             Text(text = "Hello, world!")
         }
+    }
+}
+
+@Composable
+fun ValuedImageAnimations() {
+    var alphaImage by remember {
+        mutableStateOf(0f)
+    }
+
+    val animatedAlphaImage by animateFloatAsState(
+        targetValue = alphaImage,
+        animationSpec = tween(durationMillis = 4000)
+    )
+
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .clickable { alphaImage = if (alphaImage == 0f) 1f else 0f }) {
+        Image(
+            painter = painterResource(id = R.drawable.littlelemontxt),
+            contentDescription = "",
+            modifier = Modifier.align(Alignment.Center)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.littlelemonimg),
+            contentDescription = "",
+            modifier = Modifier
+                .align(Alignment.Center)
+                .alpha(alpha = animatedAlphaImage)
+        )
     }
 }
 
