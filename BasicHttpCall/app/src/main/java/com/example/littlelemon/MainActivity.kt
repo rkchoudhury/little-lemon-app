12ae6d7139c7618a2bb27de20d899e93313c7a9a
Rakesh Choudhury <rakesh.choudhury@honeywell.com>
2023-10-07 18:50:09 +0530

    Set up ktor and fetch the data from the server

--- BasicHttpCall/app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ BasicHttpCall/app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -8,10 +8,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.lifecycleScope
 import com.example.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private var responseLiveData = MutableLiveData<String>()
+    private val httpClient = HttpClient(Android)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -19,17 +31,30 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
+                    val responseState = responseLiveData.observeAsState("").value
                     Column {
                         Button(
                             onClick = {
-
+                                lifecycleScope.launch {
+                                    val response = fetchContent()
+                                    runOnUiThread {
+                                        responseLiveData.value = response
+                                    }
+                                }
                             }
                         ) {
                             Text(text = "Download")
                         }
+                        Text(text = responseState.toString())
                     }
                 }
             }
         }
+    }
+
+    private suspend fun fetchContent(): String {
+        val response: HttpResponse =
+            httpClient.get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonMenu.json")
+        return response.bodyAsText()
     }
 }
