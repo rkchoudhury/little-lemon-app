0fd6d855f3d084180d7a20bee6502cf06b5e1ae0
Rakesh Choudhury <rakesh.choudhury@honeywell.com>
2023-10-07 11:30:45 +0530

    Added initial project

---
+++ BasicHttpCall/app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.littlelemon
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import com.example.littlelemon.ui.theme.LittleLemonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LittleLemonTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Column {
+                        Button(
+                            onClick = {
+
+                            }
+                        ) {
+                            Text(text = "Download")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
