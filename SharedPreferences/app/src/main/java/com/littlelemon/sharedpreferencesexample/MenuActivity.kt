b043b68d3444811a7e254035677a565ef5bec94e
Rakesh Choudhury <rakesh.choudhury@honeywell.com>
2023-10-27 07:28:24 +0530

    Saved user response on breakfast and lunch menu

--- SharedPreferences/app/src/main/java/com/littlelemon/sharedpreferencesexample/MenuActivity.kt
+++ SharedPreferences/app/src/main/java/com/littlelemon/sharedpreferencesexample/MenuActivity.kt
@@ -1,20 +1,46 @@
 package com.littlelemon.sharedpreferencesexample
 
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.MutableLiveData
 import com.littlelemon.sharedpreferencesexample.ui.theme.SharedPreferencesExampleTheme
 
+/*
+* Allow user to toggle between breakfast and launch menu
+* Save user response on breakfast and launch menu
+*/
 class MenuActivity : ComponentActivity() {
+
+    private val sharedPreferences by lazy {
+        getSharedPreferences("LittleLemon", MODE_PRIVATE)
+    }
+
+    private val showLunchMenuLiveData = MutableLiveData<Boolean>()
+
+    private val sharedPreferencesListener =
+        OnSharedPreferenceChangeListener { sharedPreferences, key ->
+            if (key == "LunchMenu") {
+                showLunchMenuLiveData.value = sharedPreferences.getBoolean(key, false)
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        showLunchMenuLiveData.value = sharedPreferences.getBoolean("LunchMenu", false)
+        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferencesListener)
+
         setContent {
             SharedPreferencesExampleTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +48,15 @@ class MenuActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting2("Android")
+                    Column {
+                        val selected = showLunchMenuLiveData.observeAsState(false)
+
+                        Greeting2("Save user response on breakfast and launch menu")
+                        Switch(checked = selected.value, onCheckedChange = {
+                            sharedPreferences.edit().putBoolean("LunchMenu", it).apply()
+                        })
+                        Title(showLunchMenu = selected.value)
+                    }
                 }
             }
         }
@@ -31,13 +65,17 @@ class MenuActivity : ComponentActivity() {
 
 @Composable
 fun Greeting2(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = name)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview2() {
-    SharedPreferencesExampleTheme {
-        Greeting2("Android")
+fun Title(showLunchMenu: Boolean) {
+    val text = if (showLunchMenu) {
+        "Lunch Menu"
+    } else {
+        "Breakfast Menu"
     }
+
+    Text(text = text)
 }
+
