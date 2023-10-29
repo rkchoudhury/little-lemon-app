d7164d91b6ac1339ad838c67df92fcdc2ba7d945
Rakesh Choudhury <rakesh.choudhury@honeywell.com>
2023-10-29 10:44:00 +0530

    Added `Add dish` UI

--- Room/app/src/main/java/com/littlelemon/room/MainActivity.kt
+++ Room/app/src/main/java/com/littlelemon/room/MainActivity.kt
@@ -3,13 +3,16 @@ package com.littlelemon.room
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign.Companion.Right
+import androidx.compose.ui.unit.dp
 import com.littlelemon.room.ui.theme.RoomTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +20,88 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RoomTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val menuItems by remember {
+                        mutableStateOf(emptyList<MenuItem>())
+                    }
+                    Column {
+                        var dishName by remember { mutableStateOf("") }
+                        var priceInput by remember { mutableStateOf("") }
+                        Row(modifier = Modifier.padding(16.dp)) {
+                            TextField(
+                                modifier = Modifier.weight(.6f),
+                                value = dishName,
+                                onValueChange = { value -> dishName = value },
+                                label = { Text("Dish name") }
+                            )
+                            Spacer(modifier = Modifier.width(16.dp))
+                            TextField(
+                                modifier = Modifier.weight(.4f),
+                                value = priceInput,
+                                onValueChange = { value -> priceInput = value },
+                                label = { Text("Price") }
+                            )
+                        }
+                        Button(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            onClick = {
+                                dishName = ""
+                                priceInput = ""
+                            }
+                        ) {
+                            Text("Add dish")
+                        }
+                        ItemsList(menuItems)
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RoomTheme {
-        Greeting("Android")
+    @Composable
+    private fun ItemsList(menuItems: List<MenuItem>) {
+        if (menuItems.isEmpty()) {
+            Text(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(16.dp),
+                text = "The menu is empty"
+            )
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(16.dp)
+            ) {
+                items(
+                    items = menuItems,
+                    itemContent = { menuItem ->
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            Text(menuItem.name)
+                            Text(
+                                modifier = Modifier.weight(1f),
+                                textAlign = Right,
+                                text = "%.2f".format(menuItem.price)
+                            )
+                            Spacer(modifier = Modifier.width(16.dp))
+                            Button(onClick = { /*TODO*/ }) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.delete_icon),
+                                    contentDescription = "Delete"
+                                )
+                            }
+                        }
+                    }
+                )
+            }
+        }
     }
 }
---
+++ Room/app/src/main/java/com/littlelemon/room/MenuItem.kt
@@ -0,0 +1,7 @@
+package com.littlelemon.room
+
+data class MenuItem(
+    val id: String,
+    val name: String,
+    val price: Double
+)
