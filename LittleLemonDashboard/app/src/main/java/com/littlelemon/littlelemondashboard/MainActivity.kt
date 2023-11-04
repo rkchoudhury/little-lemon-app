d7cdf53e21c22a21c166e339777d6b316d36fde0
Rakesh Choudhury <rakesh.choudhury@honeywell.com>
2023-11-04 18:01:19 +0530

    Added sort and filter functionality

--- LittleLemonDashboard/app/src/main/java/com/littlelemon/littlelemondashboard/MainActivity.kt
+++ LittleLemonDashboard/app/src/main/java/com/littlelemon/littlelemondashboard/MainActivity.kt
@@ -8,10 +8,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -19,7 +24,9 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
+import com.littlelemon.littlelemondashboard.ui.theme.Grey
 import com.littlelemon.littlelemondashboard.ui.theme.LittleLemonDashboardTheme
+import com.littlelemon.littlelemondashboard.ui.theme.Purple200
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.android.*
@@ -49,27 +56,61 @@ class MainActivity : ComponentActivity() {
                 val databaseMenuItems by database.menuItemDao().getAll().observeAsState(emptyList())
 
                 // add orderMenuItems variable here
+                val orderMenuItems = remember {
+                    mutableStateOf(false)
+                }
 
                 // add menuItems variable here
+                var menuItems = if (orderMenuItems.value) {
+                    databaseMenuItems.sortedBy { it.title }
+                } else {
+                    databaseMenuItems
+                }
 
                 Column(
                     modifier = Modifier
-                        .padding(16.dp)
+                        .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Image(
                         painter = painterResource(id = R.drawable.logo),
                         contentDescription = "logo",
                         modifier = Modifier.padding(50.dp)
                     )
-                    MenuItemsList(databaseMenuItems)
-
-                    // add Button code here
+                    Button(onClick = { orderMenuItems.value = true }) {
+                        Text(text = "Tap to Order By Name")
+                    }
 
                     // add searchPhrase variable here
+                    val searchPhrase = remember {
+                        mutableStateOf("")
+                    }
 
                     // Add OutlinedTextField
+                    OutlinedTextField(
+                        value = searchPhrase.value,
+                        onValueChange = { searchPhrase.value = it },
+                        placeholder = { Text(text = "Enter name...") },
+                        singleLine = true,
+                        label = { Text(text = "Search") },
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = Purple200,
+                            unfocusedBorderColor = Grey,
+                            unfocusedLabelColor = Grey,
+                            placeholderColor = Grey
+                        )
+                    )
 
                     // add is not empty check here
+                    if (searchPhrase.value != "") {
+                        val filteredMenuItems = menuItems.filter {
+                            it.title.lowercase().contains(searchPhrase.value.lowercase())
+                        }
+                        menuItems = filteredMenuItems
+                    }
+
+                    MenuItemsList(menuItems)
+
                 }
             }
         }
@@ -83,7 +124,8 @@ class MainActivity : ComponentActivity() {
     }
 
     private suspend fun fetchMenu(): List<MenuItemNetwork> {
-        val url = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonSimpleMenu.json"
+        val url =
+            "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonSimpleMenu.json"
         val response = httpClient.get(url).body<MenuNetwork>()
         // val response: MenuNetwork = httpClient.get(url).body() // Alternative way
         val menuItems: List<MenuItemNetwork> = response.menu ?: listOf()
