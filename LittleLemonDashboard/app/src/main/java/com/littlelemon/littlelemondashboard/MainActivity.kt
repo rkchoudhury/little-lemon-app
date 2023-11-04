9a5937a49ed46104b539402f5f8dcc1d0a910d21
Rakesh Choudhury <rakesh.choudhury@honeywell.com>
2023-11-04 10:54:35 +0530

    Added initial main activity contents

--- LittleLemonDashboard/app/src/main/java/com/littlelemon/littlelemondashboard/MainActivity.kt
+++ LittleLemonDashboard/app/src/main/java/com/littlelemon/littlelemondashboard/MainActivity.kt
@@ -3,41 +3,112 @@ package com.littlelemon.littlelemondashboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
 import com.littlelemon.littlelemondashboard.ui.theme.LittleLemonDashboardTheme
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private val httpClient = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(contentType = ContentType("text", "plain"))
+        }
+    }
+
+    private val database by lazy {
+        Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database").build()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LittleLemonDashboardTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                // add databaseMenuItems code here
+
+                // add orderMenuItems variable here
+
+                // add menuItems variable here
+
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
                 ) {
-                    Greeting("Android")
+                    Image(
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = "logo",
+                        modifier = Modifier.padding(50.dp)
+                    )
+
+                    // add Button code here
+
+                    // add searchPhrase variable here
+
+                    // Add OutlinedTextField
+
+                    // add is not empty check here
                 }
             }
         }
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            if (database.menuItemDao().isEmpty()) {
+                // add code here
+            }
+        }
+    }
+
+    private suspend fun fetchMenu(): List<MenuItemNetwork> {
+        TODO("Retrieve data")
+        // data URL: https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/littleLemonSimpleMenu.json
+    }
+
+    private fun saveMenuToDatabase(menuItemsNetwork: List<MenuItemNetwork>) {
+        val menuItemsRoom = menuItemsNetwork.map { it.toMenuItemRoom() }
+        database.menuItemDao().insertAll(*menuItemsRoom.toTypedArray())
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LittleLemonDashboardTheme {
-        Greeting("Android")
+private fun MenuItemsList(items: List<MenuItemRoom>) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(top = 20.dp)
+    ) {
+        items(
+            items = items,
+            itemContent = { menuItem ->
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(menuItem.title)
+                    Text(
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(5.dp),
+                        textAlign = TextAlign.Right,
+                        text = "%.2f".format(menuItem.price)
+                    )
+                }
+            }
+        )
     }
 }
