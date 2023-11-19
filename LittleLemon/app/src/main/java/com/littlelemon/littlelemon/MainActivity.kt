15d50b14bae82a1329b6ea5486c1346c4e8e6c5b
Rakesh Choudhury <rakesh.choudhury@honeywell.com>
2023-11-19 08:42:25 +0530

    Added the network call and fetched the menu items

--- LittleLemon/app/src/main/java/com/littlelemon/littlelemon/MainActivity.kt
+++ LittleLemon/app/src/main/java/com/littlelemon/littlelemon/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.littlelemon.littlelemon
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
 import com.littlelemon.littlelemon.navigations.AppNavigation
+import com.littlelemon.littlelemon.networks.MenuItemNetwork
+import com.littlelemon.littlelemon.networks.MenuNetwork
 import com.littlelemon.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    private val httpClient = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(contentType = ContentType("text", "plain"))
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -24,5 +43,18 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val menuItems = fetchMenu()
+            Log.d("LITTLE_LEMON", "onCreate: fetched menu items of length ${menuItems.size}")
+        }
+    }
+
+    private suspend fun fetchMenu(): List<MenuItemNetwork> {
+        val url =
+            "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
+        val response = httpClient.get(url).body<MenuNetwork>()
+
+        return response.menu ?: listOf()
     }
 }
